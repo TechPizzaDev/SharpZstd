f3187947437ea9634cac7afea8f5ff6d003da19d
TechPizza <piatkowski2002@gmail.com>
2021-12-24 18:14:06 +0100

    Added ZSTD_STATIC_LINKING_ONLY to pinvoke generator

---
+++ sources/SharpZstd/Interop/Zstd.Manual.cs
@@ -0,0 +1,20 @@
+namespace SharpZstd.Interop
+{
+    public static unsafe partial class Zstd
+    {
+        public static nuint ZSTD_COMPRESSBOUND(nuint srcSize)
+        {
+            return ((srcSize) + ((srcSize) >> 8) + (((srcSize) < (128 << 10)) ? (((128 << 10) - (srcSize)) >> 11) /* margin, from 64 to 0 */ : 0)); /* this formula ensures that bound(A) + bound(B) <= bound(A+B) as long as A and B >= 128 KB */
+        }
+
+        public static nuint ZSTD_FRAMEHEADERSIZE_PREFIX(ZSTD_format_e format)
+        {
+            return ((format) == ZSTD_format_e.ZSTD_f_zstd1 ? 5u : 1); /* minimum input size required to query frame header size */
+        }
+
+        public static nuint ZSTD_FRAMEHEADERSIZE_MIN(ZSTD_format_e format)
+        {
+            return ((format) == ZSTD_format_e.ZSTD_f_zstd1 ? 6u : 2);
+        }
+    }
+}
