09e6b866fc60401d2dd81e4c7bfbf2455b94b879
TechPizza <23627133+TechPizzaDev@users.noreply.github.com>
2023-02-11 21:31:12 +0100

    Updated osx triplet deployment targets

--- custom-triplets/x64-osx.cmake
+++ custom-triplets/x64-osx.cmake
@@ -5,3 +5,5 @@ set(VCPKG_LIBRARY_LINKAGE dynamic)
 set(VCPKG_CMAKE_SYSTEM_NAME Darwin)
 set(VCPKG_OSX_ARCHITECTURES x86_64)
 
+# Minimum supported version by .NET 7
+set(CMAKE_OSX_DEPLOYMENT_TARGET "10.15" CACHE STRING "Minimum OS X deployment version")
